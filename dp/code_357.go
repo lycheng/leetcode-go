2edd316f7829238d443324aee196f3699e1cdb26
lycheng <lycheng997@gmail.com>
2016-07-13 00:06:23 +0800

    update

---
+++ dp/code_357.go
@@ -0,0 +1,25 @@
+package dp
+
+import (
+	"fmt"
+	"math"
+)
+
+func countNumbersWithUniqueDigits(n int) int {
+	m := int(math.Min(float64(n), 10.0))
+	seq := make([]int, m+1)
+	seq[0] = 1
+
+	for i := 1; i <= m; i++ {
+		seq[i] = 9
+
+		for j := 9; j >= 9-i+2; j-- {
+			seq[i] *= j
+		}
+	}
+	result := 0
+	for _, num := range seq {
+		result += num
+	}
+	return result
+}
--- dp/dp_test.go
+++ dp/dp_test.go
@@ -21,3 +21,20 @@ func TestCode338(t *testing.T) {
 	}
 	t.Log("Success")
 }
+
+func TestCode357(t *testing.T) {
+
+	kv := map[int]int{
+		0: 1,
+		1: 10,
+		2: 91,
+	}
+
+	for k, v := range kv {
+		res := countNumbersWithUniqueDigits(k)
+		if res != v {
+			t.Errorf("Error @ (%d, %d) with output is %d", k, v, res)
+		}
+	}
+	t.Log("Success")
+}
